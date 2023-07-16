a427afcb96db0f53432afb466d309fc004fc0bdb
AVerba <57949955+AVerba@users.noreply.github.com>
2023-07-16 11:52:52 +0000

    Deploying to gh-pages from @ AVerba/Ecological-Challenges@3caafd209332ba61e4379ce6ea78ee7a10124978 🚀

---
+++ index.46a6e075.js
@@ -0,0 +1,2 @@
+new Swiper(".slider",{allowTouchMove:!0,on:{slideChange:function(){this.activeIndex,this.slides.length}}});
+//# sourceMappingURL=index.46a6e075.js.map
---
+++ index.be852b1e.js
@@ -0,0 +1,2 @@
+new Swiper(".slider",{allowTouchMove:!0,on:{slideChange:function(){this.activeIndex,this.slides.length}}});
+//# sourceMappingURL=index.be852b1e.js.map
